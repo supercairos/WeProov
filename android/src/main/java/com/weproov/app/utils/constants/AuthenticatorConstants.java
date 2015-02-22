c323579b30e4646e0b19c617cec4e9cce3615d81
Romain Caire <romain.caire@doro.com>
2015-02-22 19:27:54 +0100

    [Android] Changed IBETYA to WEPROOV

--- android/src/main/java/com/weproov/app/utils/constants/AuthenticatorConstants.java
+++ android/src/main/java/com/weproov/app/utils/constants/AuthenticatorConstants.java
@@ -5,7 +5,7 @@ package com.weproov.app.utils.constants;
  */
 public class AuthenticatorConstants {
 
-    public static final String ACCOUNT_TYPE = "com.ibetya";
+    public static final String ACCOUNT_TYPE = "com.weproov";
     public static final String AUTH_TOKEN_TYPE_FULL = "FullAccess";
     public static final String KEY_ACCOUNT_AUTHENTICATOR_FAILED = "AuhthenticatorFailed";
 }
