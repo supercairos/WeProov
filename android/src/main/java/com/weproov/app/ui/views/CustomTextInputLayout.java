63142181f1d43c40adcc867f9a0dbc99a46953fe
Romain Caire <romain.caire@doro.com>
2015-07-03 17:06:14 +0200

    [Android] Small update

--- android/src/main/java/com/weproov/app/ui/views/CustomTextInputLayout.java
+++ android/src/main/java/com/weproov/app/ui/views/CustomTextInputLayout.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.support.design.widget.TextInputLayout;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.widget.EditText;
 
@@ -18,7 +19,7 @@ public class CustomTextInputLayout extends TextInputLayout {
 	}
 
 	@Override
-	public void addView(View child, int index, android.view.ViewGroup.LayoutParams params) {
+	public void addView(final View child, int index, ViewGroup.LayoutParams params) {
 		if (child instanceof EditText) {
 			// TextInputLayout updates mCollapsingTextHelper bounds on onLayout. but Edit text is not layouted.
 			child.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
@@ -26,6 +27,7 @@ public class CustomTextInputLayout extends TextInputLayout {
 				@Override
 				public void onGlobalLayout() {
 					onLayout(false, getLeft(), getTop(), getRight(), getBottom());
+					child.getViewTreeObserver().removeOnGlobalLayoutListener(this);
 				}
 			});
 		}
